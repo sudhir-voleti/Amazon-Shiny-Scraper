e268e46c12a8aa624049ae3346906b74465604cf
yogesh1612 <yogesh1612@users.noreply.github.com>
2021-06-11 14:45:06 +0530

    .

--- dependencies.R
+++ dependencies.R
@@ -1,4 +1,4 @@
 # Install / Load relevant packages
 if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
-pacman::p_load(rvest, dplyr, tidyr, stringr,shiny,markdown)
-pacman::p_load(DT)
+pacman::p_load(rvest, dplyr, tidyr, stringr,shiny,markdown,DT)
+#pacman::p_load(DT)
